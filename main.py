6c6318ba89e17e374cfdcdd9af1b30a57be6c95a
joe-newm <j.newm500@gmail.com>
2024-03-24 11:24:33 -0500

    solved issue with sprite not being transparent

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 import player
+import sprites
 
 # pygame setup
 pygame.init()
+screen_width = 800
+screen_height = int(screen_width * 0.8)
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+# megaman sprite sheet load image
+sprite_sheet_image = pygame.image.load("sprites/player/megaman-sprite1.png").convert_alpha()
+sprite_sheet = sprites.SpriteSheet(sprite_sheet_image)
+
+frame0 = sprite_sheet.get_image(23, 25, 4)
 
 while running:
     # poll for events
@@ -16,9 +24,10 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
+    screen.fill("red")
 
     # RENDER YOUR GAME HERE
+    screen.blit(frame0, (200,200))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
---
+++ player.py
@@ -0,0 +1,6 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale, direction, speed):
+        super().__init__()
+        pass
